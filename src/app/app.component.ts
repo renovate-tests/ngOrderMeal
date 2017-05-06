7c27f6bc8ef0ac43d65c58a692c329e04b195320
yutse <ncu992201010@gmail.com>
2017-05-07 02:46:54 +0800

    消費細節撰寫

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,39 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = '訂餐資訊';
+  title = '訂餐帳本';
   arr: any = [];
   people: any[] = [];
+  infodata: any;
+  caldata: any;
 
   constructor() {
-
   }
 
   ngOnInit(): void {
     fetch('../assets/data.json', { method: 'Get' })
       .then((obj) => obj.json())
       .then((a) => this.arr = a)
-      .then(() => this.name());
+      .then(() => this.Init());
   }
 
-  name() {
+  Init() {
     const arr = this.arr as any[];
-    // console.log((this.arr as any[])
-    //   .sort(
-    //   (a, b) => this.compare(a.man, b.man)
-    //   ).map((x) => x.man)
-    // );
-
     this.people = arr.map(x => x.man)
       .filter((v, i, a) => a.indexOf(v) === i);
   }
 
   get getDate(): any[] {
     const arr = [];
-    for (let index = 0; index < 7; index++) {
+    for (let index = 0; index < 20; index++) {
       const dat = new Date('2017-1-1');
       dat.setDate(dat.getDate() + index);
-      arr.push(dat.toLocaleDateString());
+      const week = dat.getDay();
+      if (week === 0 || week === 6) { continue; }
+      arr.push(dat);
     }
     return arr;
   }
@@ -47,18 +44,33 @@ export class AppComponent implements OnInit {
     return this.people;
   }
 
-  filterData(dat, name): any {
+  filterData(dat: Date, name): any {
     const arr = this.arr as any[];
+    const item = arr.find(x => x.man === name && x.dateAt === dat.toLocaleDateString());
 
-    // console.log(arr.map(x => x.man), arr.map(x => x.dateAt), name, dat);
-    const item = arr.find(x => x.man === name && x.dateAt === dat);
-    // return item == null ? '' : item.man;
     if (item != null) {
       const money = item.beforMoney + item.inputMoney - item.spanMoney;
-      return { '目前現金': money };
+      return { 'M': '$:' + money };
     }
-
     return undefined;
+  }
+  getInfo(dat, name): any {
+    const arr = this.arr as any[];
+
+    this.caldata = arr.filter(x => x.man === name && new Date(x.dateAt) <= dat);
+    this.infodata = this.caldata.find(x => x.dateAt === dat.toLocaleDateString());
+    this.caldata = this.caldata.filter(x => new Date(x.dateAt) < dat);
+    if (this.caldata.length > 0) {
+      const inputMoneys = this.caldata.map(x => x.inputMoney).reduce((x, y) => x + y);
+      const spanMoneys = this.caldata.map(x => x.spanMoney).reduce((x, y) => x + y);
+      this.infodata.beforMoney = inputMoneys - spanMoneys;
+      console.group();
+      console.log('先前儲值總共:' + inputMoneys);
+      console.log('先前消費總共:' + spanMoneys);
+      console.groupEnd();
+    } else {
+      this.infodata.beforMoney = 0;
+    }
   }
 
   compare(a, b) {
