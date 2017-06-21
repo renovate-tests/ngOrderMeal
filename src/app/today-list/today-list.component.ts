a97092e8a028189785358414b057e702c87c8437
unknown <yu-tse@bb.com>
2017-06-21 15:53:57 +0800

    today-list 撰寫

--- src/app/today-list/today-list.component.ts
+++ src/app/today-list/today-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from "angularfire2/database";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Method } from "app/service/method";
 
 @Component({
   selector: 'app-today-list',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./today-list.component.css']
 })
 export class TodayListComponent implements OnInit {
+  form: FormGroup;
+  addNew: false;
+  todayArr = [];
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase,
+    private _fb: FormBuilder,
+  ) {
+    this.form = this._fb.group({
+      topUp: 0,
+      pay: 0,
+      store: '',
+      man: '',
+      content: ''
+    });
+  }
 
   ngOnInit() {
+    this.db.list('/todayItems').subscribe(x => {
+      this.todayArr = x;
+      console.log(x);
+    });
+  }
+
+
+  add() {
+    const new_data: UserData = this.form.value;
+    console.log(new_data);
+    const items = this.db.list('/todayItems');
+    items.push(new_data);
+    this.addNew = false;
+  }
+
+  getDateTime(item: any) {
+    if (item && item.$key) {
+      const dat = Method.GetFirebaseKeyTime(item.$key);
+      return dat.toLocaleDateString() + ' ' + dat.toLocaleTimeString();
+    }
+    return '';
+
   }
 
 }
