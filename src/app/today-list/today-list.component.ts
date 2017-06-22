5cc7105f2b215bb0306f8a275c6d56e49e02b5f1
unknown <yu-tse@bb.com>
2017-06-22 17:55:51 +0800

    today-list 調整

--- src/app/today-list/today-list.component.ts
+++ src/app/today-list/today-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { AngularFireDatabase } from "angularfire2/database";
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Method } from "app/service/method";
@@ -9,57 +9,54 @@ import { Method } from "app/service/method";
   styleUrls: ['./today-list.component.css']
 })
 export class TodayListComponent implements OnInit {
+
   form: FormGroup;
   form1: FormGroup;
-  addNew: false;
+  addNew = false;
   todayArr = [];
-
+  people = [];
+  defaultItem = {
+    topUp: 0,
+    pay: 0,
+    store: '',
+    man: '',
+    content: ''
+  };
   constructor(private db: AngularFireDatabase,
     private _fb: FormBuilder,
+    private ngZone: NgZone
   ) {
-    let defaultItem = {
-      topUp: 0,
-      pay: 0,
-      store: '',
-      man: '',
-      content: ''
-    };
-    this.form = this._fb.group(defaultItem);
-    this.form1 = this._fb.group(defaultItem);
+    this.form = this._fb.group(this.defaultItem);
+    this.form1 = this._fb.group(this.defaultItem);
   }
 
   ngOnInit() {
     this.db.list('/todayItems').subscribe(x => {
       this.todayArr = x;
-
       this.todayArr.forEach(z => {
         z.isedit = false;
         z.ischeck = false;
       })
-      // console.log(x);
     });
+    this.db.list('/people').subscribe(x => {
+      this.people = x;
+      console.log(x);
+    });
+
   }
 
-
-  add() {
-    const new_data: UserData = this.form.value;
-    console.log(new_data);
-    const items = this.db.list('/todayItems');
-    items.push(new_data);
-    this.addNew = false;
+  new_one() {
+    this.addNew = true;
+    this.form = this._fb.group(this.defaultItem);
+    setTimeout(() => { $('#dropdown').dropdown(); }, 0);
   }
-
-  update(key) {
-    const new_data: UserData = this.form1.value;
-    console.log(new_data, key);
-    const items = this.db.list('/todayItems');
-    items.update(`${key}`, new_data);
-    this.todayArr.forEach(z => z.isedit = false);
-  }
-
 
   check() {
     console.log('check');
+  }
+
+  select(man) {
+    this.form.patchValue({ man: man });
   }
 
   edit(key) {
@@ -75,10 +72,38 @@ export class TodayListComponent implements OnInit {
     this.todayArr.forEach(z => z.isedit = false);
   }
 
+  add() {
+    const new_data: UserData = this.form.value;
+    console.log(new_data);
+    this.db.list('/todayItems').push(new_data)
+      .then(() => {
+        this.addNew = false;
+        console.log('新增成功');
+      })
+      .catch(() => { console.log('新增失敗'); });
+    // 無此成員，增加成員
+    if (!R.contains(new_data.man, this.people)) {
+      this.db.list('/people').push(new_data.man);
+    }
+  }
+
+  update(key) {
+    const new_data: UserData = this.form1.value;
+    console.log(new_data, key);
+    this.db.list('/todayItems').update(`${key}`, new_data)
+      .then(() => {
+        this.todayArr.forEach(z => z.isedit = false);
+        console.log('修改成功');
+      })
+      .catch(() => { console.log('修改失敗'); });
+  }
+
   remove(key) {
     console.log('remove', key);
     if (key != null && key !== '') {
-      this.db.list('/todayItems').remove(key);
+      this.db.list('/todayItems').remove(key)
+        .then(() => { console.log('刪除成功'); })
+        .catch(() => { console.log('刪除失敗'); });
     }
   }
 
