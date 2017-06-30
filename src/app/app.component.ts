43d19179914d15dd1886087b23aa4d2e8bc6ff68
unknown <yu-tse@bb.com>
2017-06-30 18:31:44 +0800

    導列 未完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = '訂餐帳本';
+  isResponsive = false;
 
   constructor(public auth: AuthService
   ) {
